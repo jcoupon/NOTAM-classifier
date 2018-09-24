30607c0dab6ad0c09e65bfe97b89f38f9a236602
Jean Coupon <jean.coupon@gmail.com>
2018-09-24 14:05:11 +0200

    add test_processing

---
+++ python/cleaning.py
@@ -0,0 +1,13 @@
+# import all the necessary libraries
+import os
+import re
+import pandas as pd
+import numpy as np
+
+class Cleaning(object):
+    """Class to perform cleaning on data.
+    """
+
+    def __init__(self, df):
+
+        pass
