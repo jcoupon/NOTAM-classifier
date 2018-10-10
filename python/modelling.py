7679c5adb4255bc4130a84b7454942007bed7fde
Jean Coupon <jean.coupon@gmail.com>
2018-10-10 13:49:45 +0200

    add cluster predictors

--- python/modelling.py
+++ python/modelling.py
@@ -6,11 +6,14 @@ import pandas as pd
 import numpy as np
 import pickle
 
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans, AgglomerativeClustering
 from sklearn.decomposition import TruncatedSVD
+from sklearn.neighbors import KNeighborsClassifier
+
+
 
 
 # python path where the script is located
@@ -18,6 +21,11 @@ DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 # default vocabulary path
 PATH_VOCABULARY = DIR_PATH+'/vocabulary_dict.csv'
+
+# random seed
+RANDOM_STATE = None
+
+CLUSTER_MODEL_PATH = DIR_PATH+'cluster_model'
 
 """
 
@@ -33,14 +41,7 @@ class ModelTraining(object):
         """Initialization
         """
 
-        # load the dictionary in case
-        # one is found. Note: will be
-        # overwritten by build_vocabulary()
-        try:
-            self.__vocabulary_dict = read_vocabulary(
-                path=PATH_VOCABULARY)
-        except:
-            self.__vocabulary_dict = None
+        self.__vocabulary_dict = None
 
         # default number of dimensions for the word vector
         self.__n_dim = 50
@@ -62,75 +63,58 @@ class ModelTraining(object):
         self.N = len(self.__df)
         sys.stdout.write('done (found {} NOTAMs).\n'.format(self.N))
 
-    def build_vocabulary(self, path=PATH_VOCABULARY):
-        """Build a vocabulary to be used for the 
-        word count step. The input file is a csv file
-        containing a column "text_clean" out of which 
-        the vocabulary will be built.
 
-        Write the vocabulary as a dictionary where 
-        keys are terms and values are indices in 
-        the feature matrix, or an iterable over terms.
-
-        See http://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
-        """
-
-        vectorizer = CountVectorizer(stop_words='english')
-
-        # get the NOTAMS as a corpus
-        corpus = self.__df['text_clean'].fillna('').values
-
-        # compute word_counts as a
-        # sparse matrix
-        sys.stdout.write('Building and saving the dictionary...'); sys.stdout.flush()
-        vectorizer.fit(corpus)
-
-        # reset the indices so that there is 
-        # no gap between 0 and the end
-        vocabulary = {}
-        for i,(k,v) in enumerate(vectorizer.vocabulary_.items()):
-            vocabulary[k] = i
-
-        # write dictionary
-        pd.DataFrame\
-            .from_dict(vocabulary,  orient="index")\
-            .to_csv(path, header=False)
-        sys.stdout.write('done.\n'); sys.stdout.flush()
-
-        # saves the dictionary into memory
-        # Note that we could have passed 
-        # vectorizer.vocabulary_ directly 
-        # but invoking the read_vocabulary
-        # function allows us to check it
-        self.__vocabulary_dict = read_vocabulary(path=path)
-
-        return
-
-    def vectorize(self, random_state=None, n_dim=None):
+    def vectorize(
+            self, method='BOW', path_vocabulary=PATH_VOCABULARY, 
+            random_state=None, n_dim=None):
+        """Vectorize the NOTAMs and get the dictionary """
 
         if n_dim is None:
             n_dim = self.__n_dim
         self.__vector = vectorize(
-            self.__df, PATH_VOCABULARY=PATH_VOCABULARY, 
-            n_dim=n_dim, random_state=None)
+            self.__df, path_vocabulary=path_vocabulary, 
+            n_dim=n_dim, method=method, random_state=None)
 
-    def cluster_train(self, path_out='model.pickle', n_clusters=50, n_samples=None):
+        self.__vocabulary_dict = read_vocabulary(path_vocabulary)
+
+        return
+
+
+    def cluster_train(
+            self, path_out=CLUSTER_MODEL_PATH,
+            method='hierarchical', n_clusters=50, 
+            random_state=RANDOM_STATE, n_samples=None):
         """ Train clusters with hierarchical clustering 
         and persist model as the vector plus labels
         that will be used with k-NN for testing and
         predicting"""
 
-        # run hierachical clustering
+        # run clustering
         sys.stdout.write('Training (clusters)...'); sys.stdout.flush()
-        model_options_dict = {'n_clusters': n_clusters}
+        method_options_dict = {'n_clusters': n_clusters}
         model = find_clusters_train(
-            self.__vector, model_type='hierarchical',
-            model_options_dict=model_options_dict,
-            path_out=path_out, n_samples=n_samples)
+            self.__vector, method=method,
+            method_options_dict=method_options_dict,
+            path_out=path_out, n_samples=n_samples, 
+            random_state=random_state)
+        sys.stdout.write('done.\n'); sys.stdout.flush()
+
+    def cluster_predict(
+            self, path_in=CLUSTER_MODEL_PATH, 
+            method='hierarchical'):
+        # run clustering
+        sys.stdout.write('Predicting (clusters)...'); sys.stdout.flush()
+        self.__labels = find_clusters_predict(
+            self.__vector, path_in, method=method,
+            n_samples=None)
+        self.__df['cluster_labels'] = self.__labels
         sys.stdout.write('done.\n'); sys.stdout.flush()
 
     def get_vector(self):
         return self.__vector
+
+    def get_cluster_labels(self):
+        return self.__labels
 
     def get_vocabulary_dict(self):
         return self.__vocabulary_dict
@@ -153,15 +137,19 @@ class ModelPredict(object):
     on NOTAM data.
     """
 
-    def __init__(self):
-        """Options
+    def __init__(self, path=None):
+        """Initialization
         """
 
+        # default number of dimensions for the word vector
+        self.__n_dim = 50
 
-        pass
+        # read the data if a path is given
+        if path is not None:
+            self.read(path)
 
     def read(self, path):
-        """Read the clean NOTAM csv file and 
+        """Read the clean NOTAM csv file and
         load it into Pandas data frame
         """
 
@@ -173,15 +161,82 @@ class ModelPredict(object):
         self.N = len(self.__df)
         sys.stdout.write('done (found {} NOTAMs).\n'.format(self.N))
 
-    def cluster_predict(self):
+    def vectorize(
+            self, method='BOW', path_vocabulary=PATH_VOCABULARY,
+            do_build_vocabulary=False, 
+            random_state=None, n_dim=None):
+        """Vectorize the NOTAMs"""
+
+        if n_dim is None:
+            n_dim = self.__n_dim
+        self.__vector = vectorize(
+            self.__df, path_vocabulary=path_vocabulary, 
+            n_dim=n_dim, method=method, random_state=None)
+
+        return
+
+    def cluster_predict(
+            self, path_in=CLUSTER_MODEL_PATH, 
+            method='hierarchical'):
+        # run clustering
+        sys.stdout.write('Predicting (clusters)...'); sys.stdout.flush()
+        self.__cluster_labels = find_clusters_predict(
+            self.__vector, path_in, method=method,
+            n_samples=None)
+        self.__df['cluster_labels'] = self.__cluster_labels
+        sys.stdout.write('done.\n'); sys.stdout.flush()
 
 
+    def visualize(
+            self, method='t-SNE', random_state=None, 
+            path_out='clusters_TSNE.csv'):
+        """Compute the visualization tools.
 
-        pass
+        default:
+        t-SNE representation
+        additional info can be
+        found here https://distill.pub/2016/misread-tsne/
 
+
+        TODO: call plot_cluster
+        """
+
+        if method == 't-SNE':
+
+            # the perplexity drives the number of 
+            # neighbours in t-SNE. A higher number 
+            # tend to make results look smoother
+            perplexity = 100
+
+            # compute the t-SNE representation
+            # it takes about 30 mn for a sample
+            # of 20,000
+            vector_TSNE = tsne(self.__vector, random_state=random_state, perplexity=perplexity)
+
+            # persist the t-SNE coordinates
+            pd.DataFrame(vector_TSNE).to_csv(path_out)
+
+            #path_test_TSNE = path_test.replace('.csv', '_TSNE_perp{}.csv'.format(perplexity))
+
+            # the result to a file
+            #if False:
+            #else:
+            #    test_TSNE = pd.read_csv(path_test_TSNE)[['0', '1']].values
+
+
+            return
+
+
+        raise Exception('visualize(): method {} not recognized'.format(method))
+
+    def get_vector(self):
+        return self.__vector
 
     def get_df(self):
         return self.__df
+
+    def get_cluster_labels(self):
+        return self.__cluster_labels
 
     def write(self, path):
         sys.stdout.write('Writting file...')
@@ -191,8 +246,8 @@ class ModelPredict(object):
 
 def find_clusters_train(
         X, n_samples=None, persist=None, 
-        model_type='hierarchical', model_options_dict=None, 
-        path_out=None):
+        method='hierarchical', method_options_dict=None, 
+        path_out=None, random_state=None):
     """Find clusters using scikit learn
     clustering algorithms
     """
@@ -203,13 +258,12 @@ def find_clusters_train(
     else:
         choice = range(len(X))
 
-    #supress = df['supress'].values[choice]    
-    if model_type == 'kmeans':
-        if model_options_dict is None:
+    if method == 'kmeans':
+        if method_options_dict is None:
             # Default parameters
-            model = KMeans(n_clusters=4, random_state=20091982)
+            model = KMeans(n_clusters=4, random_state=random_state)
         else:
-            model = KMeans(**model_options_dict)
+            model = KMeans(**method_options_dict)
         model.fit(X[choice])
 
         # persist model
@@ -217,12 +271,12 @@ def find_clusters_train(
             with open(path_out, 'wb') as file_out:
                 pickle.dump(model, file_out)
 
-    if model_type == 'hierarchical':
-        if model_options_dict is None:
+    if method == 'hierarchical':
+        if method_options_dict is None:
             # Default parameters
             model = AgglomerativeClustering(n_clusters=7)
         else:
-            model = AgglomerativeClustering(**model_options_dict)
+            model = AgglomerativeClustering(**method_options_dict)
         model.fit(X[choice])
 
         # persist model
@@ -235,7 +289,7 @@ def find_clusters_train(
 
 def find_clusters_predict(
         X, path_in, n_samples=None, persist=None, 
-        model_type='hierarchical', model_options_dict=None):
+        method='hierarchical', method_options_dict=None):
     """Find clusters using scikit learn
     clustering algorithms
     """
@@ -247,7 +301,7 @@ def find_clusters_predict(
     else:
         choice = range(len(X))
 
-    if model_type == 'kmeans':
+    if method == 'kmeans':
         
         # read model
         with open(path_in, 'rb') as file_in:
@@ -256,14 +310,18 @@ def find_clusters_predict(
         # evaluate labels
         labels = model.predict(X[choice])
 
-    if model_type == 'hierarchical': 
+    if method == 'hierarchical': 
 
         # read model
         with open(path_in, 'rb') as file_in:
             X_train, labels_train = pickle.load(file_in)
 
+        model = KNeighborsClassifier(n_neighbors=1)
+        model.fit(X_train, labels_train)
+        labels = model.predict(X[choice])
 
     return labels
+
 
 def break_lines(input_text, stride=60):
     """Break lines for a list of texts"""
@@ -345,18 +403,30 @@ Falling back to non-interactive plot (will write plot in graph.pdf).\n')
             size = 5
             if emphasize_label is not None and emphasize_label != n:
                 size = 1
+            
+            if X_decomposed.shape[1] > 2:
+                trace = go.Scatter3d(
+                    x = X_decomposed[:, 0][select],
+                    y = X_decomposed[:, 1][select],
+                    z = X_decomposed[:, 2][select],
+                    name = label_names[n],
+                    mode = 'markers',
+                    marker = dict(size = size,),
+                    text = text[select],
+                    textposition='top left',
 
+                )
+            else:
+                trace = go.Scatter(
+                    x = X_decomposed[:, 0][select],
+                    y = X_decomposed[:, 1][select],
+                    name = label_names[n],
+                    mode = 'markers',
+                    marker = dict(size = size,),
+                    text = text[select],
+                    textposition='top left',
+                )
 
-            trace = go.Scatter(
-                x = X_decomposed[:, 0][select],
-                y = X_decomposed[:, 1][select],
-                name = label_names[n],
-                mode = 'markers',
-                marker = dict(size = size,),
-                text = text[select],
-                textposition='top left',
-
-            )
             traces.append(trace)
 
         # main layout
@@ -403,6 +473,40 @@ Falling back to non-interactive plot (will write plot in graph.pdf).\n')
     return
 
 
+def build_vocabulary(corpus, path=PATH_VOCABULARY):
+    """Build a vocabulary to be used for the 
+    word count step. The input file is a csv file
+    containing a column "text_clean" out of which 
+    the vocabulary will be built.
+
+    Write the vocabulary as a dictionary where 
+    keys are terms and values are indices in 
+    the feature matrix, or an iterable over terms.
+
+    See http://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
+    """
+
+    vectorizer = CountVectorizer(stop_words='english')
+
+    # compute word_counts as a
+    # sparse matrix
+    vectorizer.fit(corpus)
+
+    # reset the indices so that there is 
+    # no gap between 0 and the end
+    vocabulary = {}
+    for i,(k,v) in enumerate(vectorizer.vocabulary_.items()):
+        vocabulary[k] = i
+
+    # write dictionary
+    if path is not None:
+        pd.DataFrame\
+            .from_dict(vocabulary,  orient="index")\
+            .to_csv(path, header=False)
+
+    return vocabulary
+
+
 def read_vocabulary(path=PATH_VOCABULARY):
     """Read the vocabulary from path and return a dictionary.
 
@@ -427,44 +531,92 @@ def read_vocabulary(path=PATH_VOCABULARY):
     return dictionary
 
 
-def vectorize(df, PATH_VOCABULARY=PATH_VOCABULARY, n_dim=50, random_state=None):
+def vectorize(
+        df, path_vocabulary=PATH_VOCABULARY, do_build_vocabulary=True, 
+        method='BOW', text_col_name='text_clean', n_dim=50, 
+        random_state=None):
     """Vectorize the NOTAM and reduce the 
     dimensionality. First load a dictionary
     then count the words and run dimensionality
     reduction.  
     """
 
-    # load the vocabulary
-    vocabulary_dict = read_vocabulary(PATH_VOCABULARY)
-    vectorizer = CountVectorizer(stop_words='english', vocabulary=vocabulary_dict)
-
     # get the NOTAMS as a corpus
-    corpus = df['text_clean'].fillna('').values
+    corpus = df[text_col_name].fillna('').values
 
-    # compute word_counts as a
-    # sparse matrix
-    sys.stdout.write('Vectorizing the NOTAMs...')
-    word_counts = vectorizer.transform(corpus)
-    sys.stdout.write('done.\n')
+    if method == 'BOW':
+        # compute word_counts as a
+        # sparse matrix
+        sys.stdout.write('Vectorizing the NOTAMs...')
 
-    # vectorize word counts
-    # reduce dimensionality using linear PCA
-    # use TruncatedSVD which is really efficient
-    # with sparse matrices
-    sys.stdout.write('Performing dimensionality reduction...')
-    decomposer = TruncatedSVD(n_components=n_dim, random_state=random_state)
-    vector = decomposer.fit_transform(word_counts)
-    sys.stdout.write('done.\n')
+        # build or load the vocabulary
+        # usually the dictionary is built during 
+        # the training and saved
+        if do_build_vocabulary:
+            vocabulary_dict = build_vocabulary(corpus, path=path_vocabulary)
+        else:
+            vocabulary_dict = read_vocabulary(path_vocabulary)
 
-    return vector
+        vectorizer = CountVectorizer(stop_words='english', vocabulary=vocabulary_dict)
+        word_counts = vectorizer.transform(corpus)
+        sys.stdout.write('done.\n')
+
+        # vectorize word counts
+        # reduce dimensionality using linear PCA
+        # use TruncatedSVD which is really efficient
+        # with sparse matrices
+        sys.stdout.write('Performing dimensionality reduction...')
+        decomposer = TruncatedSVD(n_components=n_dim, random_state=random_state)
+        vector = decomposer.fit_transform(word_counts)
+        sys.stdout.write('done.\n')
+
+        return vector
+
+    if method == 'TF-IDF':
+        # compute word_counts as a
+        # sparse matrix
+        sys.stdout.write('Vectorizing the NOTAMs...')
+        vectorizer = TfidfVectorizer(stop_words='english')
+        word_counts = vectorizer.fit_transform(corpus)
+        sys.stdout.write('done.\n')
+
+        # vectorize word counts
+        # reduce dimensionality using linear PCA
+        # use TruncatedSVD which is really efficient
+        # with sparse matrices
+        sys.stdout.write('Performing dimensionality reduction...')
+        decomposer = TruncatedSVD(n_components=n_dim, random_state=random_state)
+        vector = decomposer.fit_transform(word_counts)
+        sys.stdout.write('done.\n')
+
+        return vector
+
+    if method == 'word2vec':
+
+        # remove common words and tokenize
+        stoplist = set('for a of the and to in'.split())
+        texts = [[word for word in document.lower().split() if word not in stoplist]
+                for document in corpus]
+        # remove words that appear only once
+        from collections import defaultdict
+        frequency = defaultdict(int)
+        for text in texts:
+            for token in text:
+                frequency[token] += 1
+
+        texts = [[token for token in text if frequency[token] > 1] for text in texts]
 
 
+        return texts
 
-def tsne(vector, n_dim=2, random_state=None):
+    raise Exception('vectorize: method {} not recognized'.format(method))
+
+
+def tsne(vector, n_dim=2, random_state=None, perplexity=100):
     """Manifold t-SNE"""
 
     result = TSNE(
-        n_components=2, verbose=2, random_state=random_state, perplexity=100)\
+        n_components=2, verbose=2, random_state=random_state, perplexity=perplexity)\
         .fit_transform(vector)
 
     return result
@@ -513,3 +665,6 @@ def get_cluster_purity(labels, classes):
     f_pure = sum(N[purity > 0.8])/sum(N)
 
     return N, purity, f_pure
+
+
+
